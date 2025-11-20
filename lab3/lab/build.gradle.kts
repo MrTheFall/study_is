06f4ad4ee0de57a3722d168edfa67f13dbee6c0a
MrTheFall <mrthefall@yandex.ru>
2025-11-20 15:19:47 +0300

    replace subprojects block with convention plugin

--- lab3/lab/build.gradle.kts
+++ lab3/lab/build.gradle.kts
@@ -1,16 +1,7 @@
-import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
-import org.gradle.api.JavaVersion
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.api.plugins.quality.CheckstyleExtension
 import org.gradle.api.tasks.Exec
-import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.api.tasks.testing.Test
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
     id("org.springframework.boot") version "3.5.6" apply false
-    id("io.spring.dependency-management") version "1.1.6" apply false
-    id("io.freefair.lombok") version "9.0.0-rc2" apply false
 }
 
 val pgContainer = providers.environmentVariable("PG_CONTAINER").orElse("orgmgr-pg")
@@ -29,52 +20,6 @@ extensions.extraProperties.apply {
     set("pgPort", pgPort.get())
     set("webPort", webPort.get())
     set("pgHost", pgHost.get())
-}
-
-subprojects {
-    group = "com.example"
-    version = "0.0.1-SNAPSHOT"
-
-    repositories {
-        mavenCentral()
-    }
-
-    apply(plugin = "java")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "io.freefair.lombok")
-    apply(plugin = "checkstyle")
-
-    extensions.configure<DependencyManagementExtension> {
-        imports {
-            mavenBom("org.springframework.boot:spring-boot-dependencies:3.5.6")
-        }
-    }
-
-    extensions.configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_21
-    }
-
-    tasks.withType<JavaCompile>().configureEach {
-        options.compilerArgs.add("-parameters")
-    }
-
-    tasks.withType<Test>().configureEach {
-        useJUnitPlatform()
-        testLogging {
-            events("passed", "skipped", "failed")
-            exceptionFormat = TestExceptionFormat.SHORT
-        }
-    }
-
-    extensions.configure<CheckstyleExtension> {
-        toolVersion = "10.18.1"
-        configDirectory.set(rootProject.layout.projectDirectory.dir("config/checkstyle"))
-    }
-
-    dependencies {
-        add("compileOnly", "com.google.code.findbugs:jsr305:3.0.2")
-        add("testCompileOnly", "com.google.code.findbugs:jsr305:3.0.2")
-    }
 }
 
 tasks.register("checkstyle") {
--- lab3/lab/services/build.gradle.kts
+++ lab3/lab/services/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("org-manager.common-conventions")
     `java-library`
 }
 
